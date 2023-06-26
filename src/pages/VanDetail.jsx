40dd6d48eb78f531bb6a3343b089620ee5753818
Kunal Rajesh Salunkhe <salunkhekunal594@gmail.com>
2023-06-26 22:00:19 +0530

    fixed: route error in production build on netlify

--- src/pages/VanDetail.jsx
+++ src/pages/VanDetail.jsx
@@ -20,42 +20,38 @@ const VanDetail = () => {
   }, [id]);
 
   return (
-    <>
+    <div className="bg-primary px-4 py-10 md:py-16">
       {vanDetails ? (
-        <div className="bg-primary px-4 py-10 md:py-16">
-          <div className="w-full sm:w-1/2 md:w-1/3 mx-auto p-4">
-            <Link to="/vans">
-              <p className="">← back to all vans</p>
-            </Link>
+        <div className="w-full sm:w-1/2 md:w-1/3 mx-auto p-4">
+          <Link to="/vans">
+            <p className="">← back to all vans</p>
+          </Link>
 
-            <img
-              className="my-6 mx-auto rounded-lg"
-              src={vanDetails.imageUrl}
-              alt=""
-            />
-            <span
-              className={`bg-${vanDetails?.color} p-2 rounded-md text-primary font-medium`}
-            >
-              {vanDetails.type}
-            </span>
-            <div className="flex flex-col gap-4 my-4">
-              <h2 className="text-xl md:text-2xl font-bold">
-                {vanDetails.name}
-              </h2>
-              <p className="text-lg md:text-xl font-semibold">
-                ${vanDetails.price}/day
-              </p>
-              <p className="text-sm md:text-lg">{vanDetails.description}</p>
-              <button className="bg-orange text-white font-medium p-2 md:text-lg rounded-sm">
-                Rent this Van
-              </button>
-            </div>
+          <img
+            className="my-6 mx-auto rounded-lg"
+            src={vanDetails.imageUrl}
+            alt=""
+          />
+          <span
+            className={`bg-${vanDetails?.color} p-2 rounded-md text-primary font-medium`}
+          >
+            {vanDetails.type}
+          </span>
+          <div className="flex flex-col gap-4 my-4">
+            <h2 className="text-xl md:text-2xl font-bold">{vanDetails.name}</h2>
+            <p className="text-lg md:text-xl font-semibold">
+              ${vanDetails.price}/day
+            </p>
+            <p className="text-sm md:text-lg">{vanDetails.description}</p>
+            <button className="bg-orange text-white font-medium p-2 md:text-lg rounded-sm">
+              Rent this Van
+            </button>
           </div>
         </div>
       ) : (
         <p>Loading...</p>
       )}
-    </>
+    </div>
   );
 };
 
