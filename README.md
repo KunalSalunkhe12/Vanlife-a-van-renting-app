8700f17ff22823531dcb5aa46a462fb2933371e3
Kunal Rajesh Salunkhe <salunkhekunal594@gmail.com>
2023-07-02 22:42:04 +0530

    feat: filter vans

--- README.md
+++ README.md
@@ -10,3 +10,6 @@
 -Relative paths
 -"." represents current path
 -".." if we want to go one route back wrt url we use `relative="path"` prop
+-searchParams - used for sorting, filtering, sharing same ui between other users
+-useSearchParams hook - return searchParams which is URLSearchParams obj which has all the params && setSearchParams which we can use to append or set new params
+-setSearchParams - takes an object which has key value pairs which are then converted into searchParams under the hood it uses URLSearchParams constructor
--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,8 +1,12 @@
 import { useState, useEffect } from "react";
 import VanCard from "../../components/VanCard";
+import { useSearchParams } from "react-router-dom";
 
 const Vans = () => {
-  const [vans, setVans] = useState(null);
+  const [vans, setVans] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const typeFilter = searchParams.get("type");
 
   const getAllVans = async () => {
     try {
@@ -18,27 +22,63 @@ const Vans = () => {
     getAllVans();
   }, []);
 
+  const displayedVans = typeFilter
+    ? vans.filter((van) => van.type === typeFilter)
+    : vans;
+
   return (
     <div className="bg-primary px-4 py-10 md:py-16">
-      <div className="sm:w-4/5 md:w-3/5 mx-auto p-4">
+      <div className="sm:w-4/5 md:w-3/5 mx-auto p-4 min-h-screen">
         <h1 className="text-2xl md:text-4xl font-bold">
           Explore our van options
         </h1>
         <div className="flex flex-wrap justify-between items-center my-10 gap-2">
-          <ul className="flex gap-3 md:gap-10 text-base md:text-lg text-primary font-medium">
-            <li className="bg-orange p-2 rounded-md cursor-pointer">Simple</li>
-            <li className="bg-black p-2 rounded-md cursor-pointer">Luxury</li>
-            <li className="bg-green p-2 rounded-md cursor-pointer">Rugged</li>
-          </ul>
-          <p className="text-sm md:text-lg cursor-pointer text-center">
-            Clear filters
-          </p>
+          <div className="flex gap-3 md:gap-10 text-base md:text-lg text-primary font-medium">
+            <button
+              className={`${
+                typeFilter === "simple"
+                  ? "bg-orange"
+                  : "bg-secondary text-black"
+              } p-2 rounded-md cursor-pointer hover:bg-orange hover:text-white`}
+              onClick={() => setSearchParams({ type: "simple" })}
+            >
+              Simple
+            </button>
+            <button
+              className={`${
+                typeFilter === "luxury" ? "bg-black" : "bg-secondary text-black"
+              } p-2 rounded-md cursor-pointer hover:bg-black hover:text-white`}
+              onClick={() => setSearchParams({ type: "luxury" })}
+            >
+              Luxury
+            </button>
+            <button
+              className={`${
+                typeFilter === "rugged" ? "bg-green" : "bg-secondary text-black"
+              } p-2 rounded-md cursor-pointer hover:bg-green hover:text-white`}
+              onClick={() => setSearchParams({ type: "rugged" })}
+            >
+              Rugged
+            </button>
+          </div>
+          {typeFilter && (
+            <button
+              onClick={() => setSearchParams({})}
+              className="text-sm md:text-lg cursor-pointer text-center"
+            >
+              Clear filters
+            </button>
+          )}
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-x-8 gap-y-14">
-          {vans ? (
-            vans.map((van) => <VanCard key={van.id} van={van} />)
+          {vans.length > 0 ? (
+            displayedVans.length > 0 ? (
+              displayedVans.map((van) => <VanCard key={van.id} van={van} />)
+            ) : (
+              <p className="text-lg font-medium">No vans to show</p>
+            )
           ) : (
-            <h1 className="min-h-screen">Loading...</h1>
+            <p className="min-h-screen">Loading...</p>
           )}
         </div>
       </div>
